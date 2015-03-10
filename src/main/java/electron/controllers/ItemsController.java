8d49386450611b1291ed0eeb27c8912e3ead15ed
Bharath MG <mgbharath@ymail.com>
2015-03-10 14:50:24 +0530

    [Abijeet, Bharath] Initial commit with FTL. Added sample home page.

---
+++ src/main/java/electron/controllers/ItemsController.java
@@ -0,0 +1,18 @@
+package electron.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@Controller
+public class ItemsController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String listItems(Map<String, Object> model) {
+        model.put("message", "Hello Electron!");
+        return "welcome";
+    }
+}
---
+++ src/main/resources/templates/welcome.ftl
@@ -0,0 +1,5 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body> ${message} </body>
+</html>
