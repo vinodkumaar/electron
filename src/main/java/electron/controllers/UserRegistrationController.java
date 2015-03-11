a012caa1be293cfd43f093f678ad1f56c1800e75
Sultanahamar Mohammad <sultanm@INsultanm.local>
2015-03-11 11:07:35 +0530

    User Registration Spikes

--- src/main/java/electron/controllers/UserRegistrationController.java
+++ src/main/java/electron/controllers/UserRegistrationController.java
@@ -3,19 +3,29 @@ package electron.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @Controller
 public class UserRegistrationController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+
+
+    @RequestMapping(value = "/user_registration", method = RequestMethod.GET)
     public String listItems(Map<String, Object> model) {
-        model.put("message", "Hello Electron!");
-        return "welcome";
+//        model.put("message", "Hello Electron!");
+        return "user_registration";
     }
 
-    @RequestMapping(value = "/home", method = RequestMethod.GET)
-    public String publicHome(Map<String, Object> model) {
+    @RequestMapping(value = "/post_registration", method = RequestMethod.POST)
+    @ResponseBody
+    public String publicHome(Map<String, Object> model,HttpServletRequest request) {
+
+
+        System.out.println(request.getParameter("dname"));
+        model.put("user", request.getParameter("dname"));
+
         return "home";
     }
 }
