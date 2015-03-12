687359fe37d8c1ad4d5a31bc26310120895bf75f
sultanm <sultanahamer@gmail.com>
2015-03-12 13:22:10 +0530

    Delivery Details / Checkout cart without user registration front end complete.

--- src/main/java/electron/Application.java
+++ src/main/java/electron/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
-class Application {
+public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
